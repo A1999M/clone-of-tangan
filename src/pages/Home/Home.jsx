9a4a57845e552e058a303d5b3fc1805818737c30
DESKTOP-T2KVN3S\Mr_Developer <molaei_Developer>
2023-05-20 03:19:19 +0430

    add navbar to home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.scss";
+import NavBar from "../../components/NavBar";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <NavBar />
+        </div>
+      </div>
+    </>
+  );
+}
