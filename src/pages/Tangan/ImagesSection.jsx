7b893da998761308bc4b40823f6b7ab5a043c080
DESKTOP-T2KVN3S\Mr_Developer <molaei_Developer>
2023-06-10 16:35:01 +0430

    responsive mage section tangan

--- src/pages/Tangan/ImagesSection.jsx
+++ src/pages/Tangan/ImagesSection.jsx
@@ -11,6 +11,32 @@ export default function ImagesSection() {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
+    if (window.innerWidth <= 500) {
+      gsap.to(rightImageRef.current, {
+        clipPath: "inset(0% 0% 0% 0%)",
+        scale: 1,
+        duration: 1.5,
+        ease: "Expo.easeOut",
+        scrollTrigger: {
+          trigger: rightImageRef.current,
+          start: "center 60%",
+          end: "bottom 0%",
+        },
+      });
+      gsap.to(leftImageRef.current, {
+        clipPath: "inset(0% 0% 0% 0%)",
+        scale: 1,
+        duration: 1.5,
+        ease: "Expo.easeOut",
+        scrollTrigger: {
+          trigger: leftImageRef.current,
+          start: "center 90%",
+          end: "bottom 0%",
+          markers: true,
+        },
+      });
+    }
+
     gsap.to(rightImageRef.current, {
       clipPath: "inset(0% 0% 0% 0%)",
       scale: 1,
@@ -29,7 +55,7 @@ export default function ImagesSection() {
       ease: "Expo.easeOut",
       scrollTrigger: {
         trigger: leftImageRef.current,
-        start: "+=1000 80%",
+        start: "+=550 80%",
         end: "bottom 0%",
       },
     });
@@ -38,8 +64,19 @@ export default function ImagesSection() {
   return (
     <>
       <div className="container-fluid images_section">
-        <div className="row">
-          <div className="col-12 col-lg-6">
+        <div className="row justify-content-end">
+          <div className="col-8 col-lg-6">
+            <div className="wrapper_right_image">
+              <img
+                ref={rightImageRef}
+                src="https://uploads-ssl.webflow.com/5bc989248743153705f137da/604101c608caef5b642fd603_seance__physioscan.jpg"
+                alt="https://uploads-ssl.webflow.com/5bc989248743153705f137da/604101c608caef5b642fd603_seance__physioscan.jpg"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-start">
+          <div className="col-6">
             <div className="wrapper_left_image">
               <img
                 ref={leftImageRef}
@@ -48,19 +85,14 @@ export default function ImagesSection() {
               />
             </div>
           </div>
-          <div className="col-12 col-lg-6">
-            <div className="wrapper_right_image">
-              <img
-                ref={rightImageRef}
-                src="https://uploads-ssl.webflow.com/5bc989248743153705f137da/604101c608caef5b642fd603_seance__physioscan.jpg"
-                alt="https://uploads-ssl.webflow.com/5bc989248743153705f137da/604101c608caef5b642fd603_seance__physioscan.jpg"
+          <div className="col-6">
+            <div className="btn_imageSection">
+              <Btn
+                borderColor="#000"
+                firstText="makean appointment"
+                secondText="TAKE A BREAK"
               />
             </div>
-            <Btn
-              borderColor="#000"
-              firstText="makean appointment"
-              secondText="TAKE A BREAK"
-            />
           </div>
         </div>
       </div>
